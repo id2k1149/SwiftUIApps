9c3d418a2e4b82bfbdaf89f234aa8fe1a14304ca
alfie <id2k1149@gmail.com>
2026-03-16 17:36:43 +0100

    start RandomColorApp

---
+++ RandomColorApp/RandomColorApp/RandomColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorAppApp.swift
+//  RandomColorApp
+//
+//  Created by Alfred Thaddeus Crane Pennyworth on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct RandomColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
