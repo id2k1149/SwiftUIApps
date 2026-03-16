7c1f74585778e325a1cb3fab401ec8d0b3beffc3
alfie <id2k1149@gmail.com>
2026-03-16 18:57:00 +0100

    dice roller app

---
+++ DiceRollerApp/DiceRollerApp/DiceRollerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerAppApp.swift
+//  DiceRollerApp
+//
+//  Created by Alfred Thaddeus Crane Pennyworth on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
