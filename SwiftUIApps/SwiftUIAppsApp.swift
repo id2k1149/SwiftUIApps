f47f45a1c94da2ab0d6874bf06a2357d500183fa
alfie <id2k1149@gmail.com>
2026-03-16 16:46:33 +0100

    Initial Commit

---
+++ SwiftUIApps/SwiftUIAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppsApp.swift
+//  SwiftUIApps
+//
+//  Created by Alfred Thaddeus Crane Pennyworth on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
