790f9fdfdc9ac76d25b3fbc2038b9b3c85bf07fe
alfie <id2k1149@gmail.com>
2026-03-16 18:58:51 +0100

    content view commit

--- DiceRollerApp/DiceRollerApp/ContentView.swift
+++ DiceRollerApp/DiceRollerApp/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diceNumber = 1 // текущее число кубика
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Text("Dice Roller")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Image("dice\(diceNumber)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+
+            Button(action: {
+                // Генерация случайного числа от 1 до 6
+                diceNumber = Int.random(in: 1...6)
+            }) {
+                Text("Roll Dice")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 15)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .clipShape(Capsule())
+                    .overlay(
+                        Capsule()
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+            }
         }
         .padding()
     }
