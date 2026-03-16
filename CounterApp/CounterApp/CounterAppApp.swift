053a5b17b42c26f5044e42e64e546e279c2a2b91
alfie <id2k1149@gmail.com>
2026-03-16 17:11:46 +0100

    Counter App

---
+++ CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Alfred Thaddeus Crane Pennyworth on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
