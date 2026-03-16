444055ed5c3fe50e0a46c57d1d85df58d0cc8174
alfie <id2k1149@gmail.com>
2026-03-16 20:19:39 +0100

    2 dices added

--- DiceRollerApp/DiceRollerApp/ContentView.swift
+++ DiceRollerApp/DiceRollerApp/ContentView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var diceNumber = 1
-    @State private var rotation = 0.0
+    @State private var dice1 = 1
+    @State private var dice2 = 1
+    
+    @State private var rotation1 = 0.0
+    @State private var rotation2 = 0.0
+    
+    @State private var offset1 = 0.0
+    @State private var offset2 = 0.0
+    
     @State private var scale: CGFloat = 1.0
 
     var body: some View {
@@ -18,7 +25,8 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
 
-            Image("dice\(diceNumber)")
+            /*
+            Image("dice\(dice1)")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 200, height: 200)
@@ -28,26 +36,62 @@ struct ContentView: View {
                 // shadow
                 .shadow(radius: 10)
                 .animation(.easeOut(duration: 0.6), value: rotation)
+             */
+            
+            HStack(spacing: 0) {
+                
+                Image("dice\(dice1)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150)
+                        .rotationEffect(.degrees(rotation1))
+                        .scaleEffect(scale)
+                        .offset(x: -offset1)
+                        .shadow(radius: 10)
+                
+                Image("dice\(dice2)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150)
+                        .rotationEffect(.degrees(rotation2))
+                        .scaleEffect(scale)
+                        .offset(x: -offset2)
+                        .shadow(radius: 10)
+            }
+            .animation(.easeOut(duration: 0.7), value: rotation1)
+            .animation(.easeOut(duration: 0.9), value: rotation2)
 
             Button(action: {
 
                 // новое случайное число
-                diceNumber = Int.random(in: 1...6)
+                dice1 = Int.random(in: 1...6)
+                dice2 = Int.random(in: 1...6)
 
                 // добавляем вращение
 //                rotation += 360
 //                rotation += Double.random(in: 720...1440)
-                let spins = Double.random(in: 360...720)
-                let direction = Bool.random() ? 1.0 : -1.0
+                
+                let spins1 = Double.random(in: 360...720)
+                let spins2 = Double.random(in: 360...720)
+                
+                let direction1 = Bool.random() ? 1.0 : -1.0
+                let direction2 = Bool.random() ? 1.0 : -1.0
 
-                rotation += spins * direction
+                rotation1 += spins1 * direction1
+                rotation2 += spins2 * direction2
                 
                 //*
                 // подпрыгивание
-                scale = 1.4
+                scale = 1.5
+                
+                // разъезжаются
+                offset1 = -60
+                offset2 = 60
 
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                     scale = 1.0
+                    offset1 = 0
+                    offset2 = 0
                 }
                 // */
 
